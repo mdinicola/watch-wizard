a95157092b0803b56793786f55583a6d1b6cbdbc
m4cola <mike@mdinicola.com>
2022-07-17 19:41:09 -0400

    update google actions handler

--- watch_wizard/handlers/google_actions_handler.py
+++ watch_wizard/handlers/google_actions_handler.py
@@ -2,9 +2,20 @@ from clients.google_actions import format_response
 from models.movie import Movie
 import json
 
-def recommend_movie_and_speak(event, context):
+def handle_response(event, context):
     request_data = json.loads(event['body'])
+    handler_name = request_data['handler']['name']
+    
+    if handler_name == 'recommend_movie':
+        return recommend_movie(request_data)
+    else:
+        message = f'Handler name {handler_name} is not valid'
+        return {
+            'statusCode': 400,
+            'body': json.dumps(message)
+        }
 
+def recommend_movie(request_data):
     recommendation_message = Movie.get_recommendation_message()
     response = format_response(request_data, recommendation_message)
 
