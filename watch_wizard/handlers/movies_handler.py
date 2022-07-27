c532f5ba109ebec633267f16b0cbb9d474718d4d
m4cola <mike@mdinicola.com>
2022-07-27 15:37:31 -0400

    add missing file

---
+++ watch_wizard/handlers/movies_handler.py
@@ -0,0 +1,29 @@
+from common.enhanced_json_encoder import EnhancedJSONEncoder
+from clients.trakt_client import TraktClient
+from os import environ
+import json
+import logging
+
+AWS_SECRET_NAME = environ['TraktSecretName']
+TRAKT_CLIENT_ID_KEY = 'CLIENT_ID'
+TRAKT_CLIENT_SECRET_KEY = 'CLIENT_SECRET'
+
+_logger = logging.getLogger(__name__)
+
+def recommend_movie(event, context):
+    try:
+        trakt_client = TraktClient(AWS_SECRET_NAME)
+        movie = trakt_client.get_recommended_movie()
+   
+        return {
+            'statusCode': 200,
+            'body': json.dumps(movie, cls=EnhancedJSONEncoder)
+        }
+
+    except Exception as e:
+        _logger.exception(e)
+        message = 'An unexpected error ocurred.  See log for details'
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': message})
+        }
