24695c87c641fce3075be9f356824fde94475f23
m4cola <mike@mdinicola.com>
2022-07-30 21:06:12 -0400

    verity skill id matches request

--- watch_wizard/handlers/alexa_skills_handler.py
+++ watch_wizard/handlers/alexa_skills_handler.py
@@ -4,10 +4,12 @@ from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
+from clients.aws_secrets_manager import SecretsManagerSecret
 from handlers import movies_handler
 
 from os import environ
 import ask_sdk_core.utils as ask_utils
+import boto3
 import json
 import logging
 
@@ -146,31 +148,42 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
                 .response
         )
 
+_AWS_SECRET_NAME = environ['ServiceSecretName']
+_ALEXA_SKILL_ID_KEY = 'AlexaSkillId'
+secret = SecretsManagerSecret(client = boto3.client('secretsmanager'), secret_name = _AWS_SECRET_NAME)
+
 # The SkillBuilder object acts as the entry point for your skill, routing all request and response
 # payloads to the handlers above. Make sure any new handlers or interceptors you've
 # defined are included below. The order matters - they're processed top to bottom.
 
-sb = SkillBuilder()
+_skill_builder = SkillBuilder()
 
-sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(RecommendMovieIntentHandler())
-sb.add_request_handler(HelpIntentHandler())
-sb.add_request_handler(CancelOrStopIntentHandler())
-sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+_skill_builder.add_request_handler(LaunchRequestHandler())
+_skill_builder.add_request_handler(RecommendMovieIntentHandler())
+_skill_builder.add_request_handler(HelpIntentHandler())
+_skill_builder.add_request_handler(CancelOrStopIntentHandler())
+_skill_builder.add_request_handler(SessionEndedRequestHandler())
+_skill_builder.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
 
-sb.add_exception_handler(CatchAllExceptionHandler())
+_skill_builder.add_exception_handler(CatchAllExceptionHandler())
+
+def set_skill_id():
+    skill_id = secret.get_value(_ALEXA_SKILL_ID_KEY)
+    # SkillBuilder will verify the skill id matches the request from Alexa before routing to handlers
+    _skill_builder.skill_id = skill_id
 
 def handle_skill_request(event, context):
-    handler = sb.lambda_handler()
+    set_skill_id()
+    handler = _skill_builder.lambda_handler()
     return handler(event, context)
 
 def handle_api_request(event, context):
+    set_skill_id()
     # skip verification if testing locally
     if environ.get('AWS_SAM_LOCAL') == 'true':
-        webservice_handler = WebserviceSkillHandler(skill=sb.create(), verify_signature=False, verify_timestamp=False)
+        webservice_handler = WebserviceSkillHandler(skill=_skill_builder.create(), verify_signature=False, verify_timestamp=False)
     else:
-        webservice_handler = WebserviceSkillHandler(skill=sb.create())
+        webservice_handler = WebserviceSkillHandler(skill=_skill_builder.create())
 
     response = webservice_handler.verify_request_and_dispatch(event['headers'], event['body'])
     
