05390ad2634dc61038ba71b6922505c2c2eceb0c
Mike Di Nicola <mike@mdinicola.com>
2024-06-23 11:20:39 -0400

    merge secrets

--- envs/local/start_localstack.sh
+++ envs/local/start_localstack.sh
@@ -4,6 +4,6 @@
 
 localstack start --detached
 
-aws --endpoint-url http://localhost:4566 secretsmanager create-secret --name apps/WatchWizard-Trakt --secret-string "{\"CLIENT_ID\": \"${TRAKTCLIENTID}\", \"CLIENT_SECRET\": \"${TRAKTCLIENTSECRET}\", \"OAUTH_TOKEN\": \"${TRAKTOAUTHTOKEN}\", \"OAUTH_REFRESH\": \"${TRAKTOAUTHREFRESH}\", \"OAUTH_EXPIRES_AT\": ${TRAKTOAUTHEXPIRESAT}}"
+aws --endpoint-url http://localhost:4566 secretsmanager create-secret --name apps/WatchWizard --secret-string "{\"AlexaSkillId\": \"${ALEXASKILLID}\", \"PlexUsername\": \"${PLEXUSERNAME}\", \"PlexPassword\": \"${PLEXPASSWORD}\", \"PlexServerName\": \"${PLEXSERVERNAME}\", \"TraktClientId\": \"${TRAKTCLIENTID}\", \"TraktClientSecret\": \"${TRAKTCLIENTSECRET}\"}"
 
-aws --endpoint-url http://localhost:4566 secretsmanager create-secret --name apps/WatchWizard --secret-string "{\"AlexaSkillId\": \"${ALEXASKILLID}\", \"PlexUsername\": \"${PLEXUSERNAME}\", \"PlexPassword\": \"${PLEXPASSWORD}\", \"PlexServerName\": \"${PLEXSERVERNAME}\"}"
+# aws --endpoint-url http://localhost:4566 secretsmanager create-secret --name apps/WatchWizard --secret-string "{\"AlexaSkillId\": \"${ALEXASKILLID}\", \"PlexUsername\": \"${PLEXUSERNAME}\", \"PlexPassword\": \"${PLEXPASSWORD}\", \"PlexServerName\": \"${PLEXSERVERNAME}\", \"TraktClientId\": \"${TRAKTCLIENTID}\", \"TraktClientSecret\": \"${TRAKTCLIENTSECRET}\", \"TraktOauthToken\": \"${TRAKTOAUTHTOKEN}\", \"TraktOauthRefreshToken\": \"${TRAKTOAUTHREFRESHTOKEN}\", \"TraktOauthExpiryData\": \"${TRAKTOAUTHEXPIRYDATE}\"}"
