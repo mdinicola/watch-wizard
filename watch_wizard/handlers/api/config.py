4391a868b537fb316fd92050472ea90bfb3b4e03
Mike Di Nicola <mike@mdinicola.com>
2024-06-23 20:48:40 -0400

    remove config get function from api events

--- watch_wizard/handlers/api/config.py
+++ watch_wizard/handlers/api/config.py
@@ -10,19 +10,13 @@ _config_service = ConfigService()
 
 def get_config(event, context) -> dict:
     try:
-        data = {
-            'trakt': _config_service.trakt_config,
-            'plex': _config_service.plex_config,
-            'alexa': _config_service.alexa_config,
-            'config': _config_service
-        }
         return {
             'statusCode': 200,
-            'body': json.dumps(data, cls=EnhancedJSONEncoder)
+            'body': json.dumps(_config_service, cls=EnhancedJSONEncoder)
         }
     except Exception as e:
         _logger.exception(e)
-        message = 'Plex connection unsuccessful.  See log for details'
+        message = 'Unable to retrieve configuration.  See log for details'
         return {
             'statusCode': 500,
             'body': json.dumps({'message': message})
